91b4b101f00374781bce2feb0168ac1390f0e3c5
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-01-09 10:45:25 -0700

    Removed solution code

--- src/App.js
+++ src/App.js
@@ -9,31 +9,19 @@ import AppNotFound from './components/AppNotFound'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      list: ["frog", "dog", "cat", "monkey"]
-    }
+  state = {
+    list: ["frog", "dog", "cat", "monkey"]
   }
 
   render() {
     return (
-      <Router>
-        <div className="nav">
 
-          <Link className="link" to="/">Home</Link>
-          <Link className="link" to="/about/">About</Link>
-          <Link className="link" to="/list/">List</Link>
-        </div>
-        <Switch>
-          <Route exact path="/" component={ Home } />
-          <Route path="/about/" component={ About } />
-          <Route
-            path="/list/"
-            render={ (props) => <List { ...props } list={ this.state } /> } />
-          <Route component={ AppNotFound } />
-        </Switch>
-      </Router>
+      <div className="nav">
+        <h1>Router Lesson</h1>
+        {/* <Link className="link" to="/">Home</Link>
+        <Link className="link" to="/about/">About</Link>
+        <Link className="link" to="/list/">List</Link> */}
+      </div>
     );
   }
 }
--- src/components/List.js
+++ src/components/List.js
@@ -3,41 +3,20 @@ import React, { Component } from 'react';
 import { Redirect, withRouter } from 'react-router-dom'
 
 class List extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            toHome: false
-        }
-    }
-
-    handleRedirect = () => {
-        this.setState({
-            toHome: true
-        })
-    }
-
-    handleHistoryPush = () => {
-        this.props.history.push("/")
-    }
-
-
 
     render() {
-        console.log("history props", this.props.history)
-        if (this.state.toHome === true) {
-            return <Redirect to="/" />
-        }
+        // console.log("history props", this.props.history)
 
         const animalList = this.props.list.list.map((animal, idx) => {
-            return <h1 key={ idx }>{ animal }</h1>
+            return <h1 key={idx}>{animal}</h1>
         })
         return (
             <div className="list">
-                { animalList }
+                {animalList}
 
-                <button onClick={ this.handleRedirect }>Redirect Component Button</button>
+                <button>Redirect Component Button</button>
 
-                <button onClick={ this.handleHistoryPush }>History Push Button</button>
+                <button>History Push Button</button>
             </div >
 
         );
